fb1a1b33f00c17fdfc8ce74b2e4fa6b4d19ef2f1
Skander <skander.kc@gmail.com>
2021-04-26 03:10:54 -0400

    Add templates

--- server/__init__.py
+++ server/__init__.py
@@ -10,12 +10,18 @@ app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URI
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+
 # ==== HTML =====
 @app.route('/')
 def dashboard_view():
     return render_template('index.html')
 
-# ==== API =====
 
+@app.route('/battery/<int:battery_id>')
+def battery_view(battery_id):
+    return render_template('battery.html', battery_id=battery_id)
+
+
+# ==== API =====
 if __name__ == '__main__':
     app.run()
