edb819013bf13e6c9fc4ccbd7b90a12ac216632e
Tinynja <minou-98-@hotmail.com>
2021-07-01 00:55:23 -0400

    Fixed syntax for shell ( [[ to [ )

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 # Set default db path if not set
-if [[ -z "$DB_PATH" ]]; then
+if [ -z "$DB_PATH" ]; then
   DB_PATH="./db/battery.db"
 fi
 
 # Check if DB file exists
-if [[ ! -f "$DB_PATH" ]]; then
+if [ ! -f "$DB_PATH" ]; then
   python3 manage.py createdb
 fi
 
